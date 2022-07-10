58a8a26dc78c2a74bc91bdd39204ae5b8ce87a34
laterNaT <105680553+laterNaT@users.noreply.github.com>
2022-07-10 15:55:53 +0200

    Implement addding books to list

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
 let myLibrary = [];
 
-
 function Book(title, author, pageNr, isRead) {
   this.title = title;
   this.author = author;
   this.pageNr = pageNr;
   this.isRead = isRead;
 
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${pageNr} pages, ${isRead ? 'is already read' : 'not read yet'}`;
-  }
+  // this.info = function() {
+  //   return `${this.title} by ${this.author}, ${pageNr} pages, ${isRead ? 'is already read' : 'not read yet'}`;
+  // }
 }
 
 function addBookToLibrary(title, author, pageNr, isRead) {
   let newBook = new Book(title, author, pageNr, isRead);
   myLibrary.push(newBook);
+
+  // Add book to table
+  let lastTr = document.querySelector('table tbody tr:last-child');
+  if (!lastTr) {
+    // if no tr added, select body instead
+    lastTr = document.querySelector('table tbody');
+  }
+
+  const tr = document.createElement('tr');
+  lastTr.after(tr);
+  for (const key in newBook) {
+    if (Object.prototype.hasOwnProperty.call(newBook, key)) {
+      const td = document.createElement('td');
+      td.innerText = newBook[key];
+      tr.appendChild(td);
+    }
+  }
 }
 
 const addBookBtn = document.getElementById('add-book-btn');
@@ -31,3 +47,14 @@ const cancelBtn = document.querySelector('.input-wrapper:last-child button:last-
     formWrapper.style.visibility = 'visible';
   }
 }));
+
+const addBookForm = document.getElementById('add-book-form');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const title = document.getElementById('bookTitle').value;
+  const author = document.getElementById('bookAuthor').value;
+  const bookPages = document.getElementById('bookPages').value;
+  const bookIsRead = document.getElementById('bookIsRead').checked;
+  addBookToLibrary(title, author, bookPages, bookIsRead);
+  addBookForm.reset();
+});
